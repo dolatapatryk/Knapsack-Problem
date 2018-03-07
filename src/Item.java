8adcc636d39ad7ad629a9fa3e8d2b46fe3f801b6
Patryk Dolata <dolata.patryk1@gmail.com>
2018-03-07 23:37:12 +0100

    first commit

---
+++ src/Item.java
@@ -0,0 +1,46 @@
+
+public class Item {
+	int key;
+	int value;
+	int weight;
+	double ratio;
+	
+	public Item(int key, int value, int weight, double ratio) {
+		this.key = key;
+		this.value = value;
+		this.weight = weight;
+		this.ratio = ratio;
+	}
+
+	public int getKey() {
+		return key;
+	}
+
+	public void setKey(int key) {
+		this.key = key;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	public double getRatio() {
+		return ratio;
+	}
+
+	public void setRatio(double ratio) {
+		this.ratio = ratio;
+	}
+}
